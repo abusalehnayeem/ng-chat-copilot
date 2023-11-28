d72082447c5f3932a08de201b9125cfd75e4e9b3
Abusaleh Md Nayeem <abusaleh.nayeem@hana-ltd.com>
2023-11-28 12:51:13 +0600

    initial comment

---
+++ Ng.Chat.Copilot.Api/Controllers/ChatController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ng.Chat.Copilot.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+    }
+}
