27e0117590908998dda6664960be2cc30b4c3857
Abusaleh Md Nayeem <abusaleh.nayeem@hana-ltd.com>
2024-03-20 17:08:37 +0600

    added local llm successfully

--- Ng.Chat.Copilot.Console/Program.cs
+++ Ng.Chat.Copilot.Console/Program.cs
@@ -1,59 +1,65 @@
 ﻿using LLama;
-using LLama.Abstractions;
 using LLama.Common;
-using System.Text;
 
 var @params = new ModelParams("D:\\Personal-Projects\\llm-test\\ng-chat-copilot\\ai-model\\zephyr-7b-beta.Q6_K.gguf");
 
 using var weights = LLamaWeights.LoadFromFile(@params);
 
-// Create 2 contexts sharing the same weights
-using var aliceCtx = weights.CreateContext(@params);
-var alice = new InteractiveExecutor(aliceCtx);
-using var bobCtx = weights.CreateContext(@params);
-var bob = new InteractiveExecutor(bobCtx);
+// Create context
+using var llamaCtx = weights.CreateContext(@params);
+var executor = new InteractiveExecutor(llamaCtx);
+//var chatSession = new ChatSession(executor);
 
-// Initial alice prompt
-var alicePrompt = "Transcript of a dialog, where the Alice interacts a person named Bob. Alice is friendly, kind, honest and good at writing.\nAlice: Hello";
-var aliceResponse = await Prompt(alice, ConsoleColor.Green, alicePrompt, false, false);
+// show the prompt
+var prompt =
+    @"Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, 
+kind, honest, good at writing, 
+and never fails to answer the User's requests immediately and with precision.\r\n\r\nUser: Hello, Bob.\r\nBob: Hello. How may I help you today?\r\nUser: Please tell me the largest city in Europe.\r\nBob: Sure. The largest city in Europe is Moscow, the capital of Russia.\r\nUser:"; // use the "chat-with-bob" prompt here.
+Console.WriteLine();
+Console.Write(prompt);
 
-// Initial bob prompt
-var bobPrompt = $"Transcript of a dialog, where the Bob interacts a person named Alice. Bob is smart, intellectual and good at writing.\nAlice: Hello{aliceResponse}";
-var bobResponse = await Prompt(bob, ConsoleColor.Red, bobPrompt, true, true);
+// run the inference in a loop to chat with LLM
+var inferenceParams = new InferenceParams
+{
+    Temperature = 0.9f,
+    AntiPrompts = new List<string> { "User:" },
+    MaxTokens = 500
+};
 
-// swap back and forth from Alice to Bob
+
 while (true)
 {
-    aliceResponse = await Prompt(alice, ConsoleColor.Green, bobResponse, false, true);
-    bobResponse = await Prompt(bob, ConsoleColor.Red, aliceResponse, false, true);
+    await foreach (var text in executor.InferAsync(prompt, inferenceParams)) Console.Write(text);
 
-    if (Console.KeyAvailable)
-        break;
-}
-
-async Task<string> Prompt(ILLamaExecutor executor, ConsoleColor color, string prompt, bool showPrompt, bool showResponse)
-{
-    var inferenceParams = new InferenceParams
-    {
-        Temperature = 0.9f,
-        AntiPrompts = new List<string> { "Alice:", "Bob:", "User:" },
-        MaxTokens = 128,
-        Mirostat = MirostatType.Mirostat2,
-        MirostatTau = 10,
-    };
-
+    Console.ForegroundColor = ConsoleColor.Green;
+    prompt = Console.ReadLine();
     Console.ForegroundColor = ConsoleColor.White;
-    if (showPrompt)
-        Console.Write(prompt);
-
-    Console.ForegroundColor = color;
-    var builder = new StringBuilder();
-    await foreach (var text in executor.InferAsync(prompt, inferenceParams))
-    {
-        builder.Append(text);
-        if (showResponse)
-            Console.Write(text);
-    }
-
-    return builder.ToString();
 }
+
+
+//async Task<string> Prompt(ILLamaExecutor executor, ConsoleColor color, string prompt, bool showPrompt, bool showResponse)
+//{
+//    var inferenceParams = new InferenceParams
+//    {
+//        Temperature = 0.9f,
+//        AntiPrompts = new List<string> { "Alice:", "Bob:", "User:" },
+//        MaxTokens = 128,
+//        Mirostat = MirostatType.Mirostat2,
+//        MirostatTau = 10,
+//    };
+
+//    Console.ForegroundColor = ConsoleColor.White;
+//    if (showPrompt)
+//        Console.Write(prompt);
+
+//    Console.ForegroundColor = color;
+//    var builder = new StringBuilder();
+//    await foreach (var text in executor.InferAsync(prompt, inferenceParams))
+//    {
+//        builder.Append(text);
+//        if (showResponse)
+//            Console.Write(text);
+//    }
+
+//    return builder.ToString();
+//}
